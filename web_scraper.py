5b50478a3f4ac0de8e3583e0ecd9de6bc5d8f146
caprico99278 <t.matsuoka@system-cpx.com>
2024-02-27 18:19:50 +0900

    テキスト出力対応

--- web_scraper.py
+++ web_scraper.py
@@ -1,14 +1,29 @@
-"""Webスクレイピングツールテスト"""
-# import packages
-import urllib.request
+"""Webスクレイピングツールプロトタイプ"""
+
+# パッケージのインポート
+import requests
 from bs4 import BeautifulSoup
 
-# the targeted URL
+# スクレイピング対象のサイトのURL
 url = 'https://www.yelp.com/biz/milk-and-cream-cereal-bar-new-york?osq=Ice+Cream'
 
-# use request to open the URL
-ourUrl = urllib.request.urlopen(url)
+# サイトを開いて、開いたページのHTMLを取得
+ourUrl = requests.get(url, timeout=60)
 
-soup = BeautifulSoup(ourUrl, 'html.parser')
+# 文字化け防止
+ourUrl.encoding = ourUrl.apparent_encoding
 
-print(soup.prettify())
+# Webスクレイピング用オブジェクトへ変換
+soup = BeautifulSoup(ourUrl.text, 'html.parser')
+
+# 変換結果をファイルに書き出す
+with open('myfile.txt', mode='w', encoding='utf-8') as f:
+    items = []
+    html_item_cards = soup.select('[data-testid="menu-item-tag"]')
+
+    for html_item_card in html_item_cards:
+
+        # TODO:抜き出すべきタグとテキスト情報の選定
+        item = {}
+        items.append(item)
+        f.write(html_item_card.get_text() + '\n')
